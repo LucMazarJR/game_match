de004d5ad6eae5bb1739168900e7edbbfb427e43
LucianoMazar <lmazaraojr@gmail.com>
2026-04-05 17:00:19 -0300

    feat: configure routes and layou interaction

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Games } from './games/games';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Games}
+];
---
+++ src/app/games/games.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-games',
+  imports: [],
+  templateUrl: './games.html',
+})
+export class Games {}
