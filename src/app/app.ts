31a16798c109a4bff064c7b645d63b14890d0ec2
LucianoMazar <lmazaraojr@gmail.com>
2026-04-05 16:53:02 -0300

    feat: finish header design

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import { LucideGamepad2 } from '@lucide/angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLinkWithHref],
+  imports: [RouterOutlet, RouterLinkWithHref, LucideGamepad2],
   templateUrl: './app.html',
 })
 
