be86048df9aff14718d5d4f0ac88a7c2780a703e
LucianoMazar <lmazaraojr@gmail.com>
2026-04-07 08:31:30 -0300

    feat(form): add selection logic in plataforms

--- src/app/games/match/form/form.ts
+++ src/app/games/match/form/form.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, output, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { catchError, finalize, of } from 'rxjs';
 import { FormsApi } from './forms-api';
@@ -7,12 +7,16 @@ import { FormsApi } from './forms-api';
   selector: 'app-form',
   imports: [],
   templateUrl: './form.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Form {
   private formsApi = inject(FormsApi);
 
   public loading = signal(true);
   public errorMessage = signal<string | null>(null);
+
+  readonly selectedPlataformIds = input<number[]>([]);
+  readonly selectedPlataformsChange = output<number[]>();
 
   public plataforms = toSignal(
     this.formsApi.getGamesPlataforms().pipe(
@@ -24,4 +28,13 @@ export class Form {
     ),
     { initialValue: [] },
   );
+
+  protected togglePlataform(plataformId: number) {
+    const currentIds = this.selectedPlataformIds();
+    const nextIds = currentIds.includes(plataformId)
+      ? currentIds.filter((id) => id !== plataformId)
+      : [...currentIds, plataformId];
+
+    this.selectedPlataformsChange.emit(nextIds);
+  }
 }
--- src/app/games/match/match.ts
+++ src/app/games/match/match.ts
@@ -4,6 +4,7 @@ import { Form } from './form/form';
 
 export type formApi = {
   selectedGenreIds: number[];
+  selectedPlataformIds: number[];
 };
 
 @Component({
@@ -12,13 +13,23 @@ export type formApi = {
   templateUrl: './match.html',
 })
 export class Match {
-  protected readonly formApiData = signal<formApi>({ selectedGenreIds: [] });
+  protected readonly formApiData = signal<formApi>({
+    selectedGenreIds: [],
+    selectedPlataformIds: [],
+  });
   protected readonly formPage = signal<number>(1); //Valor padrão = 0, 1 para dev
 
   protected onSelectedGenresChange(ids: number[]) {
     this.formApiData.update((current) => ({
       ...current,
       selectedGenreIds: ids,
+    }));
+  }
+
+  protected onSelectedPlataformsChange(ids: number[]) {
+    this.formApiData.update((current) => ({
+      ...current,
+      selectedPlataformIds: ids,
     }));
   }
 }
